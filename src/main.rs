1c8d3a6dc5e5117a0049dfcd0f905f71b9ea6c37
Nestor <nesmb16@gmail.com>
2023-06-22 13:55:24 -0600

    Hopefully resolved blocking on parsing problem

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,47 @@
-use std::sync::{Arc,Mutex};
-use tower_http::services::ServeDir;
 use axum::{
-    Router,Server,
-    routing::{get,post}
+    routing::{get, post},
+    Router, Server,
 };
+use std::sync::Arc;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    dotenv::dotenv().ok(); 
+    dotenv::dotenv().ok();
 
-    let (tx, _ ) = tokio::sync::broadcast::channel::<String>(1); 
-    let raw = Arc::new(Mutex::new(String::new()));
-    let rendered = Arc::new(Mutex::new(String::new()));
+    let (raw_tx, mut raw_rx): (
+        tokio::sync::broadcast::Sender<String>,
+        tokio::sync::broadcast::Receiver<String>,
+    ) = tokio::sync::broadcast::channel(100);
 
-    let app_state = study_buddy::server::MarkdownState{tx, raw, rendered};
+    let (html_tx, _) = tokio::sync::broadcast::channel(100);
+
+    let app_state = Arc::new(study_buddy::server::AppState::new(raw_tx, html_tx.clone()));
+
+    tokio::task::spawn(async move {
+        loop {
+            if let Ok(raw_markdown) = raw_rx.recv().await {
+                let _ = html_tx.send(study_buddy::parse_markdown_file(&raw_markdown));
+            }
+        }
+    });
 
     let router = Router::new()
         .route("/", get(study_buddy::server::get_root))
         .route("/refresh", get(study_buddy::server::refresh_file))
-        .route("/download",post(study_buddy::server::download_current_markdown))
+        .route(
+            "/download",
+            post(study_buddy::server::download_current_markdown),
+        )
         .nest_service("/static", ServeDir::new("static"))
         .with_state(app_state);
 
-    let server = Server::bind(&"0.0.0.0:0".parse().
-                              expect("Address is guaranteed to be valid")
-                              ).serve(router.into_make_service());
+    let server = Server::bind(
+        &"0.0.0.0:0"
+            .parse()
+            .expect("Address is guaranteed to be valid"),
+    )
+    .serve(router.into_make_service());
 
     println!("Listening on: {:?}", server.local_addr());
 
--- src/server.rs
+++ src/server.rs
@@ -1,71 +1,92 @@
-use std::sync::{Arc,Mutex};
-use crate::parse_markdown_file;
-use serde::{Deserialize,Serialize};
 use axum::{
-    extract::{ws::{WebSocket,Message}, WebSocketUpgrade, State},
-    response::{Html, Json, Response, IntoResponse},
     body::StreamBody,
+    extract::{
+        ws::{Message, WebSocket},
+        State, WebSocketUpgrade,
+    },
+    response::{Html, IntoResponse, Json, Response},
 };
-use tokio::{
-    fs::read_to_string,
-    sync::broadcast,
-};
+use futures::{sink::SinkExt, stream::StreamExt};
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+use tokio::{fs::read_to_string, sync};
 
-#[derive(Clone)]
-pub struct MarkdownState{
-    pub raw : Arc<Mutex<String>>,
-    pub rendered : Arc<Mutex<String>>,
-    pub tx : broadcast::Sender<String>,
+pub struct AppState {
+    pub raw_tx: sync::broadcast::Sender<String>,
+    pub markdown_tx: sync::broadcast::Sender<String>,
+    pub markdown: Mutex<String>,
+}
+
+impl AppState {
+    pub fn new(
+        raw_tx: sync::broadcast::Sender<String>,
+        markdown_tx: sync::broadcast::Sender<String>,
+    ) -> Self {
+        let markdown = Mutex::new(String::new());
+
+        AppState {
+            raw_tx,
+            markdown,
+            markdown_tx,
+        }
+    }
 }
 
 pub async fn get_root() -> Html<String> {
     Html(read_to_string("static/html/index.html").await.unwrap())
 }
 
-pub async fn refresh_file(ws: WebSocketUpgrade, State(state): State<MarkdownState>) -> Response {
+pub async fn refresh_file(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> Response {
     ws.on_upgrade(|socket| modify_md_file_state(socket, state))
 }
 
-pub async fn modify_md_file_state(mut socket: WebSocket, state : MarkdownState) {
- 
-    while let Some(new_md_file_state) = socket.recv().await {
+pub async fn modify_md_file_state(socket: WebSocket, state: Arc<AppState>) {
+    let (mut sender, mut reciever) = socket.split();
 
-        let new_md_file_state = if let Ok(file_state) = new_md_file_state {
-            file_state
-        } else {
-            return;
-        };
+    let mut rx = state.markdown_tx.subscribe();
 
-        if let Message::Text(file_state) = new_md_file_state {
-
-            *state.raw.lock().unwrap() = file_state.clone();
-
-            if socket.send(Message::Text(parse_markdown_file(&file_state))).await.is_err() {
-                return;
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(rendered) = rx.recv().await {
+            if sender.send(Message::Text(rendered)).await.is_err() {
+                break;
             }
         }
-    }
+    });
+
+    let mut recieve_task = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(raw_markdown))) = reciever.next().await {
+            let _ = state.raw_tx.send(raw_markdown.clone());
+            {
+                *state.markdown.lock().unwrap() = raw_markdown;
+            }
+        }
+    });
+
+    tokio::select! {
+           _ = (&mut send_task) => recieve_task.abort(),
+           _ = (&mut recieve_task) => send_task.abort(),
+    };
 }
 
-
-#[derive(Deserialize,Debug)]
+#[derive(Deserialize, Debug)]
 pub struct PDFDownloadRequest {
-    html : String,
-    css : String,
+    html: String,
+    css: String,
 }
 
-pub async fn download_current_markdown(Json(html_json_payload): Json<PDFDownloadRequest>) -> impl IntoResponse{
+pub async fn download_current_markdown(
+    Json(html_json_payload): Json<PDFDownloadRequest>,
+) -> impl IntoResponse {
+    println!("{:?}", html_json_payload);
 
-    println!("{:?}",html_json_payload);
-
-    #[derive(Serialize,Debug, Default)]
+    #[derive(Serialize, Debug, Default)]
     struct ApiRequest {
-        html : String,
-        css : String,
-        js : String,
+        html: String,
+        css: String,
+        js: String,
     }
 
-    enum StyleType{
+    enum StyleType {
         Light,
         Dark,
     }
@@ -75,7 +96,7 @@ pub async fn download_current_markdown(Json(html_json_payload): Json<PDFDownload
 
         fn try_from(value: &str) -> Result<Self, Self::Error> {
             match value {
-                "dark" =>  Ok(StyleType::Dark),
+                "dark" => Ok(StyleType::Dark),
                 "light" => Ok(StyleType::Light),
                 _ => Err(format!("Style {} not supported", value)),
             }
@@ -89,25 +110,26 @@ pub async fn download_current_markdown(Json(html_json_payload): Json<PDFDownload
             StyleType::Dark => include_str!("../static/templates/pdf.css"),
             StyleType::Light => include_str!("../static/templates/lightpdf.css"),
         }
-    }else {
+    } else {
         include_str!("../static/templates/pdf.css")
-    }.to_string();
-
+    }
+    .to_string();
 
     let html = format!("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><link href=\"https://pvinis.github.io/iosevka-webfont/3.4.1/iosevka.css\" rel=\"stylesheet\"/><link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.css\" integrity=\"sha384-3UiQGuEI4TTMaFmGIZumfRPtfKQ3trwQE2JgosJxCnGmQpL/lJdjpcHkaaFwHlcI\" crossorigin=\"anonymous\"/><title>StudyBuddyDownload</title></head><body><div>{}</div></body></html>",html_json_payload.html);
-     
+
     let js = include_str!("../static/templates/pdf.js").to_string();
 
-    let api_request = ApiRequest{html, css, js};
+    let api_request = ApiRequest { html, css, js };
     let api_url = "https://api.pdfendpoint.com/v1/convert";
     let mut headers = reqwest::header::HeaderMap::new();
     let auth_key = std::env::var("PDF_API_KEY").expect("PDF_API_KEY must be set");
     let auth_string = format!("Bearer {}", auth_key);
 
-    let auth = reqwest::header::HeaderValue::from_str(&auth_string).expect("Auth key is valid ASCII");
+    let auth =
+        reqwest::header::HeaderValue::from_str(&auth_string).expect("Auth key is valid ASCII");
     let content_type = reqwest::header::HeaderValue::from_static("application/json");
-    headers.insert(reqwest::header::CONTENT_TYPE,content_type);
-    headers.insert(reqwest::header::AUTHORIZATION,auth);
+    headers.insert(reqwest::header::CONTENT_TYPE, content_type);
+    headers.insert(reqwest::header::AUTHORIZATION, auth);
 
     //Make POST request
 
@@ -117,26 +139,30 @@ pub async fn download_current_markdown(Json(html_json_payload): Json<PDFDownload
         .post(api_url)
         .headers(headers)
         .json(&api_request)
-        .send().await;
+        .send()
+        .await;
 
     match api_response {
         Ok(response) => {
-           
             let stream = response.bytes_stream();
             let body = StreamBody::new(stream);
 
             let headers = [
                 (axum::http::header::CONTENT_TYPE, "application/pdf"),
-                (axum::http::header::CONTENT_DISPOSITION,
-                 "attachment; filename=StudyBuddyDownload.pdf"
+                (
+                    axum::http::header::CONTENT_DISPOSITION,
+                    "attachment; filename=StudyBuddyDownload.pdf",
                 ),
             ];
 
-            Ok((headers,body))
+            Ok((headers, body))
         }
         Err(err) => {
             //TODO better error handling
-            return Err((axum::http::StatusCode::NOT_FOUND, format!("Error performing conversion {}", err)));
+            return Err((
+                axum::http::StatusCode::NOT_FOUND,
+                format!("Error performing conversion {}", err),
+            ));
         }
     }
 }
